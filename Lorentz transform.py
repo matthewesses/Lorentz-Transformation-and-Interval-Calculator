93ccb49205bee897cb4bd5229a244a771f33d474
matthewesses <77927566+matthewesses@users.noreply.github.com>
2021-02-21 12:41:55 -0600

    Update Lorentz transform.py

--- Lorentz transform.py
+++ Lorentz transform.py
@@ -1,15 +1,14 @@
 import numpy as np
-#edit the variables to get the correct values
-x=2.4*10**8 #meters for moving reference frame
-t=1 #seconds for moving reference frame
+x=10 # length for unmoving reference frame
+t=40 # time for unmoving reference frame
 y=0 #meters
 z=0 #meters
 xp= None #xprime
 tp= None #tprime
 yp=y
 zp=z
-c=3*10**8 #meters per second
-v=None #meters per second
+c=1 #length per time
+v=None #length per time
 if v==None:
     if x!=None and t!=None:
         v=x/t
@@ -30,14 +29,20 @@ elif t==None and xp==None:
 elif x==None and tp==None:
     x=(tp*gamma+(B*t*gamma*gamma))/((1+(B*gamma)**2))
     tp=gamma*(t-B*x/c)
-I=x**2+(c*t)**2+y**2+z**2 #the value for the interval
+I=0-x**2+(c*t)**2-y**2-z**2 #the value for the interval
 
 #prints the values
-print("Distance x: ",x,"meters")
-print("Time t:", t,"seconds")
-print("Velocity is", v, "meters/second")
+print("Distance x: ",x)
+print("Time t:", t,)
+print("Velocity is:", v)
 print("Beta is",B)
 print("Gamma is", gamma)
-print("The interval is", I, "meters squared")
-print("Distance x': ",xp,"meters")
-print("Time t':", tp,"seconds")
+print("The interval is", I)
+if I<0:
+    print("spacelike")
+if I>0:
+    print("timelike")
+elif I==0:
+    print("lightike")
+print("Distance x' : ",xp,)
+print("Time t':", tp)
